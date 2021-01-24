7c7ce9cb83e939196f66611af998bbb0297febe9
Carolina R <carolinarizk@gmail.com>
2021-01-24 12:00:26 -0500

    added local storage functions(getItem & setItem)

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=05c2a7d4eefaaf927737607c9fd27a6c",
+//onclick event for the search button
+$("#searchBtn").on("click", function () {
+  //button getting value from input box
+  var city = $(".inputCity").val();
+  //ajax call
+  $.ajax({
+    url:
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=05c2a7d4eefaaf927737607c9fd27a6c",
     method: "GET",
-crossDomain: true,
+    crossDomain: true,
   }).then(function (response) {
     console.log(response);
+    storeCity(city);
   });
+});
+
+//access local storage to keeps that have been searched
+//store city function
+function storeCity(city) {
+  //notice this var is scoped only in this function -not same as getCities function below
+  var storedCities = getCities();
+  storedCities.push(city);
+  localStorage.setItem("storeCity", JSON.stringify(storedCities));
+}
+//when is data stored? user types in input box & clicks search btn.
+//set a max for searched cities stored (10 would be good)
+//store array using JSON stringify
+
+function getCities() {
+  var storedCities = localStorage.getItem("storeCity");
+  //this will give back cities array
+  return JSON.parse(storedCities) || [];
+}
